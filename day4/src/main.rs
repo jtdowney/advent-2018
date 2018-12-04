f2c70825a4a1204a1882bd7f753a8fef0a306c29
John Downey <jdowney@gmail.com>
2018-12-04 09:18:40 -0600

    Cleanup day 4
    This is blatantly ripped from @drewolson's elixir code.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,7 +4,6 @@ use failure::Error;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::ops::Range;
 use std::str::FromStr;
 
 #[derive(Debug, Copy, Clone)]
@@ -60,53 +59,27 @@ impl FromStr for Event {
     }
 }
 
-fn part1(input: &[(u16, Range<u8>)]) {
-    let (target, _) = input
+fn part1(schedule: &HashMap<u16, HashMap<u8, usize>>) {
+    let (&guard, counts) = schedule
         .iter()
-        .fold(HashMap::new(), |mut acc, (guard, sleep_time)| {
-            *acc.entry(*guard).or_insert(0) +=
-                u16::from(sleep_time.end) - u16::from(sleep_time.start);
-            acc
-        }).into_iter()
-        .max_by_key(|&(_, sleep_time)| sleep_time)
+        .max_by_key(|(_, counts)| counts.values().sum::<usize>())
         .unwrap();
 
-    let shifts = input
-        .iter()
-        .filter(|(guard, _)| *guard == target)
-        .collect::<Vec<_>>();
+    let (&minute, _) = counts.iter().max_by_key(|&(_, count)| count).unwrap();
 
-    let (minute, _) = (0..60)
-        .fold(HashMap::new(), |mut acc, minute| {
-            *acc.entry(minute).or_insert(0) += shifts
-                .iter()
-                .filter(|(_, sleep_time)| minute >= sleep_time.start && minute < sleep_time.end)
-                .count();
-            acc
-        }).into_iter()
-        .max_by_key(|&(_, count)| count)
-        .unwrap();
-
-    let answer = u32::from(target) * u32::from(minute);
+    let answer = u32::from(guard) * u32::from(minute);
     println!("part 1: {}", answer);
 }
 
-fn part2(input: &[(u16, Range<u8>)]) {
-    let ((target, minute), _) = (0..60)
-        .fold(HashMap::new(), |mut acc, minute| {
-            input
-                .iter()
-                .filter(|(_, sleep_time)| minute >= sleep_time.start && minute < sleep_time.end)
-                .for_each(|&(guard, _)| {
-                    *acc.entry((guard, minute)).or_insert(0) += 1;
-                });
-
-            acc
-        }).into_iter()
-        .max_by_key(|&(_, count)| count)
+fn part2(schedule: &HashMap<u16, HashMap<u8, usize>>) {
+    let (&guard, counts) = schedule
+        .iter()
+        .max_by_key(|(_, counts)| counts.values().max())
         .unwrap();
 
-    let answer = u32::from(target) * u32::from(minute);
+    let (&minute, _) = counts.iter().max_by_key(|&(_, count)| count).unwrap();
+
+    let answer = u32::from(guard) * u32::from(minute);
     println!("part 2: {}", answer);
 }
 
@@ -120,12 +93,18 @@ fn main() -> Result<(), Error> {
     input.sort_by_key(|e| (e.year, e.month, e.day, e.hour, e.minute));
 
     let (schedule, _, _) = input.iter().fold(
-        (vec![], 0, 0),
+        (HashMap::new(), 0, 0),
         |(mut acc, current_guard, start_sleep), event| match event.action {
             Action::StartShift(guard) => (acc, guard, 0),
             Action::FallAsleep => (acc, current_guard, event.minute),
             Action::WakeUp => {
-                acc.push((current_guard, start_sleep..event.minute));
+                for minute in start_sleep..event.minute {
+                    *acc.entry(current_guard)
+                        .or_insert_with(HashMap::new)
+                        .entry(minute)
+                        .or_insert(0) += 1;
+                }
+
                 (acc, current_guard, 0)
             }
         },
