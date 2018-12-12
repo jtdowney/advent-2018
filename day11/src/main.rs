4bdeb831b1e104f5abe20803d13cdce548f4b3cd
John Downey <jdowney@gmail.com>
2018-12-12 05:22:48 -0600

    Rewrite day 11 part 2 with a better algorithm

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -3,45 +3,10 @@ extern crate itertools;
 use itertools::iproduct;
 use std::collections::HashMap;
 
-const INPUT: usize = 5535;
+const INPUT: isize = 5535;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
-struct Point(usize, usize);
-
-struct Scorer<'a> {
-    grid: &'a HashMap<Point, isize>,
-    cache: HashMap<(Point, usize), isize>,
-}
-
-impl<'a> Scorer<'a> {
-    fn new(grid: &'a HashMap<Point, isize>) -> Scorer<'a> {
-        Scorer {
-            grid,
-            cache: HashMap::new(),
-        }
-    }
-
-    fn score(&mut self, point: Point, size: usize) -> Option<isize> {
-        if let Some(&score) = self.cache.get(&(point, size)) {
-            return Some(score);
-        }
-
-        if size == 1 {
-            return self.grid.get(&point).cloned();
-        }
-
-        let parts = partition_space(point, size);
-        let score = parts
-            .iter()
-            .map(|&(point, size)| self.score(point, size))
-            .collect::<Option<Vec<isize>>>()?
-            .iter()
-            .sum();
-        self.cache.insert((point, size), score);
-
-        Some(score)
-    }
-}
+struct Point(isize, isize);
 
 fn power_level(Point(x, y): Point) -> isize {
     let rack_id = x + 10;
@@ -58,52 +23,17 @@ fn power_level(Point(x, y): Point) -> isize {
     hundreds - 5
 }
 
-fn partition_space(point: Point, size: usize) -> Vec<(Point, usize)> {
-    if size == 1 {
-        return vec![(point, size)];
-    }
-
-    let mut parts = vec![];
-    let Point(x, y) = point;
-    let left = size / 2;
-    let right = size - left;
-    parts.push((point, left));
-    parts.push((Point(x + left, y + left), right));
-
-    if left != right {
-        let min = left.min(right);
-        let max = left.max(right);
-        let edge = max - min;
-        parts.push((Point(x, y + left), min));
-        parts.push((Point(x + left, y), min));
-
-        for i in 0..min {
-            parts.push((Point(x + i, y + left + min), edge));
-            parts.push((Point(x + left + min, y + i), edge));
-        }
-    } else {
-        parts.push((Point(x, y + left), right));
-        parts.push((Point(x + left, y), right));
-    }
-
-    parts
-}
-
-fn create_grid() -> HashMap<Point, isize> {
-    iproduct!(1..=300, 1..=300)
-        .map(|(x, y)| {
-            let point = Point(x, y);
-            let power_level = power_level(point);
-            (point, power_level)
-        })
-        .collect()
-}
-
-fn part1(scorer: &mut Scorer) {
-    let (Point(x, y), _) = iproduct!(1..=300, 1..=300)
-        .map(|(x, y)| Point(x, y))
-        .filter_map(|start| {
-            let score = scorer.score(start, 3)?;
+fn part1(grid: &HashMap<Point, isize>) {
+    let (Point(x, y), _) = grid
+        .keys()
+        .filter_map(|&start| {
+            let Point(x, y) = start;
+            let score: isize = iproduct!(0..3, 0..3)
+                .map(|(dx, dy)| Point(x + dx, y + dy))
+                .map(|point| grid.get(&point).cloned())
+                .collect::<Option<Vec<isize>>>()?
+                .iter()
+                .sum();
             Some((start, score))
         })
         .max_by_key(|&(_, score)| score)
@@ -112,23 +42,48 @@ fn part1(scorer: &mut Scorer) {
     println!("part 1: {},{}", x, y);
 }
 
-fn part2(scorer: &mut Scorer) {
-    let (Point(x, y), size, _) = iproduct!(1..=300, 1..=300, 1..=300)
-        .map(|(x, y, size)| (Point(x, y), size))
-        .filter_map(|(start, size)| {
-            let score = scorer.score(start, size)?;
-            Some((start, size, score))
+fn part2(grid: &HashMap<Point, isize>) {
+    let cache = iproduct!(1..=300, 1..=300).map(|(x, y)| Point(x, y)).fold(
+        HashMap::new(),
+        |mut acc, point| {
+            let Point(x, y) = point;
+            let prev = acc.get(&Point(x, y - 1)).cloned().unwrap_or_default();
+            let row: isize = (1..=x).map(|i| grid[&Point(i, y)]).sum();
+
+            let score: isize = prev + row;
+
+            acc.insert(point, score);
+            acc
+        },
+    );
+
+    let (Point(x, y), offset) = iproduct!(1..=300, 1..=300)
+        .flat_map(|(x, y)| {
+            let end = 300 - x.max(y);
+            (0..=end).map(move |i| (Point(x, y), i))
         })
-        .max_by_key(|&(_, _, score)| score)
+        .max_by_key(|(Point(x, y), i)| {
+            let target = cache[&Point(x + i, y + i)];
+            let top = cache.get(&Point(x + i, y - 1)).cloned().unwrap_or_default();
+            let side = cache.get(&Point(x - 1, y + i)).cloned().unwrap_or_default();
+            let overage = cache.get(&Point(x - 1, y - 1)).cloned().unwrap_or_default();
+
+            target - top - side + overage
+        })
         .unwrap();
 
-    println!("part 2: {},{},{}", x, y, size);
+    println!("part 2: {},{},{}", x, y, offset + 1);
 }
 
 fn main() {
-    let grid = create_grid();
-    let mut scorer = Scorer::new(&grid);
+    let grid = iproduct!(1..=300, 1..=300)
+        .map(|(x, y)| {
+            let point = Point(x, y);
+            let power_level = power_level(point);
+            (point, power_level)
+        })
+        .collect();
 
-    part1(&mut scorer);
-    part2(&mut scorer);
+    part1(&grid);
+    part2(&grid);
 }
